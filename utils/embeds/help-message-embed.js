ff09586050724b0004cb7abcb9f5d56af7f1361e
tlaskey <tlaskey3@gmail.com>
2020-07-07 01:54:49 -0700

    run standard

--- utils/embeds/help-message-embed.js
+++ utils/embeds/help-message-embed.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 
 class HelpMessageEmbed extends Discord.MessageEmbed {
-  constructor() {
+  constructor () {
     super()
       .setColor('#34BEBA')
       .setTitle('JanMichaelBot')
